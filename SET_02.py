c575b52a783d3d8c6f333069481ed116961ab4b9
pmahajan <m.prasshantmahajan@gmail.com>
2022-08-23 01:36:14 +0530

    API_TRAINING

--- SET_02.py
+++ SET_02.py
@@ -1,8 +1,20 @@
-@app.route('/inserts',methods = ['POST'])
+from flask import Flask, request, jsonify
+import mysql.connector as conn
+
+app=Flask(__name__)
+mydb=conn.connect(host="localhost", user="root", passwd="India@2022")
+cursor=mydb.cursor()
+cursor.execute(" create database if not exists Rajveer ")
+cursor.execute(" create table if not exists Rajveer.table1(name varchar(30), mob_no int) ")
+
+@app.route('/insert',methods = ['POST'])
 def insert():
-    if request.method=='POST':
+    if request.method == 'POST':
         name = request.json['name']
-        number = request.json['number']
-        cursor.execute("insert into connection001.tasktable  values(%s , %s)",(name ,number))
+        mob_no = request.json['mob_no']
+        cursor.execute(" insert into Rajveer.table1 values(%s,  %s)" ,(name,mob_no))
         mydb.commit()
-        return jsonify(str('succesfully inserted'))
+        return jsonify(str('succes'))
+
+if __name__== '__main__':
+    app.run()
